0b422ff8d9cef3b58d179e54c1047ced70f2efa3
Jim Weirich <jim.weirich@gmail.com>
2009-10-13 18:00:48 -0400

    Unit tests now written in given

--- test/given/test_unit/adapter_test.rb
+++ test/given/test_unit/adapter_test.rb
@@ -13,31 +13,20 @@ class AdapterTest < Given::Contract
   Given do
     When { given_assert(lambda { true }) }
     Then { @assertion_counted }
-  end
 
-  def test_given_fails_with_file_and_line_with_false_block
-    line = "xx"
-    ex = assert_raise(Test::Unit::AssertionFailedError) do
-      line = __LINE__ + 1
+    When {
+      @line = __LINE__ + 1
       given_assert(lambda { false })
-    end
-    assert_match(/#{__FILE__}:#{line}/, ex.message)
-  end
+    }
+    Fails(Test::Unit::AssertionFailedError) {
+      @exception.message =~ /#{__FILE__}:#{@line}/
+    }
 
-
-  Given do
     When {
       given_assert(lambda { fail "OUCH" })
     }
     Fails(RuntimeError) {
       @exception.message == "OUCH"
     }
-  end
-  
-  def test_given_fails_with_actual_exception
-    ex = assert_raise(RuntimeError) do
-      given_assert(lambda { fail "OUCH" })
-    end
-    assert_equal "OUCH", ex.message
   end
 end

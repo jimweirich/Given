5b6d8596aa3bcd76458afb06be39457a2f58fd80
Jim Weirich <jim.weirich@gmail.com>
2009-10-16 23:43:15 -0400

    Allowing use of TestCase or TestCaseMethods

--- lib/given/test_unit.rb
+++ lib/given/test_unit.rb
@@ -3,10 +3,18 @@ require 'test/unit'
 require 'given/test_unit/adapter'
 
 module Given
+  module TestCaseMethods
+    def self.included(mod)
+      mod.module_eval do
+        extend Given::DSL
+        include Given::DSL::TestHelper
+        include Given::TestUnit::Adapter
+      end
+    end
+  end
+
   class TestCase < Test::Unit::TestCase
-    extend Given::DSL
-    include Given::DSL::TestHelper
-    include Given::TestUnit::Adapter
+    include Given::TestCaseMethods
 
     def test_DUMMY
     end

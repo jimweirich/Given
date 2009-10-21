9cff0481042283077b81f9ecb05fa49c6a22213c
Jim Weirich <jim.weirich@gmail.com>
2009-10-21 11:12:58 -0400

    Added comments (and minor tweaks) to the Greed Score Example

--- examples/greed_score_test.rb
+++ examples/greed_score_test.rb
@@ -1,13 +1,36 @@
 require 'test/unit'
 require 'given/test_unit'
 
+# Greed is a dice game.  The rules to Greed varies, depending on who
+# you talk to, but the rules we are using are:
+#
+# * You can roll up to 5 dice
+#
+# * Any triplet of ones is worth 1000
+# 
+# * Any triplet of any other face value is worth 100 times the face
+#   value (e.g. 3 fours is worth 400 points)
+#
+# * Any ones not used in a triplet are worth 100 points
+#
+# * Any fives not used in a triplet are worth 50 points.
+#
+# * Any 2, 3, 4, or 6 faces not used in a triplet are worth 0 points
+#   are are counted as unused dice.
+#
 module Greed
+  # Score:
+  # * Tracks the points for a roll of dice
+  # * Reports the points for a roll
+  # * Reports the number of unused faces in a roll.
   class Score
     attr_reader :points, :unused
 
-    def self.for(dice)
+    # Return the score object for a roll.  A roll is a list of
+    # integers representing the faces showing after a roll of dice.
+    def self.for(roll)
       @score = new
-      @score.score(dice)
+      @score.score(roll)
       @score
     end
 
@@ -20,15 +43,19 @@ module Greed
       "score<#{points},#{unused}>"
     end
 
+    def inspect
+      to_s
+    end
+
     def ==(other)
       other.kind_of?(Greed::Score) &&
         points == other.points &&
         unused == other.unused
     end
 
-    def score(dice)
+    def score(roll)
       (1..6).each do |face|
-        score_face(count_faces(face, dice), face)
+        score_face(count_faces(face, roll), face)
       end
     end
 
@@ -58,7 +85,7 @@ module Greed
     end
 
     def count_faces(face, dice)
-      dice.select { |f| f == face}.size
+      dice.select { |f| f == face }.size
     end
 
   end
@@ -66,23 +93,23 @@ end
 
 
 class GreedScoreTest < Given::TestCase
-  def score(dice)
+  def points(dice)
     @score = Greed::Score.for(dice)
     expect(@score.points)
   end
 
   Given do
-    Then { score([]) == 0 }
-    Then { score([5]) == 50 }
-    Then { score([5,5]) == 100 }
-    Then { score([1]) == 100 }
-    Then { score([1,1]) == 200 }
-    Then { score([1,5,2,3,6]) == 150 }
-    Then { score([1,1,1]) == 1000 }
-    Then { score([2,2,2]) == 200 }
-    Then { score([3,3,3]) == 300 }
-    Then { score([1,1,1,1]) == 1100 }
-    Then { score([5,5,5,5]) == 550 }
+    Then { points([]) == 0 }
+    Then { points([5]) == 50 }
+    Then { points([5,5]) == 100 }
+    Then { points([1]) == 100 }
+    Then { points([1,1]) == 200 }
+    Then { points([1,5,2,3,6]) == 150 }
+    Then { points([1,1,1]) == 1000 }
+    Then { points([2,2,2]) == 200 }
+    Then { points([3,3,3]) == 300 }
+    Then { points([1,1,1,1]) == 1100 }
+    Then { points([5,5,5,5]) == 550 }
   end
 
   def unused(dice)

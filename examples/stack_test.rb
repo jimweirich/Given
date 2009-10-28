573a70318b76a5cdf9e94a4fb58efe5819b4b2dc
Jim Weirich <jim.weirich@gmail.com>
2009-10-28 11:03:56 -0400

    Modified examples to the assert2 form

--- examples/stack_test.rb
+++ examples/stack_test.rb
@@ -2,23 +2,23 @@ require 'given/test_unit'
 require 'examples/stack'
 
 class StackBehavior < Given::TestCase
-  Invariant { expect(@stack.depth) >= 0 }
-  Invariant { expect(@stack.empty?) == (@stack.depth == 0) }
+  Invariant { @stack.depth >= 0 }
+  Invariant { @stack.empty? == (@stack.depth == 0) }
 
   def empty_stack
     @stack = Stack.new
   end
 
   Given(:empty_stack) do
-    Then { expect(@stack.depth) == 0 }
+    Then { @stack.depth == 0 }
 
     When { @stack.push(:an_item) }
-    Then { expect(@stack.depth) == 1 }
-    Then { expect(@stack.top) == :an_item }
+    Then { @stack.depth == 1 }
+    Then { @stack.top == :an_item }
 
     When { @stack.pop }
     FailsWith(Stack::UsageError)
-    Then { expect(exception.message) =~ /empty/ }
+    Then { exception.message =~ /empty/ }
   end
 
   def stack_with_two_items
@@ -28,19 +28,19 @@ class StackBehavior < Given::TestCase
   end
 
   Given(:stack_with_two_items) {
-    Then { expect(@stack.top) == :top_item }
-    Then { expect(@stack.depth) == 2}
+    Then { @stack.top == :top_item }
+    Then { @stack.depth == 2}
 
     When { @result = @stack.pop }
-    Then { expect(@result) == :top_item }
-    Then { expect(@stack.top) == :bottom_item }
-    Then { expect(@stack.depth) == 1 }
+    Then { @result == :top_item }
+    Then { @stack.top == :bottom_item }
+    Then { @stack.depth == 1 }
 
     When {
       @stack.pop
       @result = @stack.pop
     }
-    Then { expect(@result) == :bottom_item }
-    Then { expect(@stack.depth) == 0 }
+    Then { @result == :bottom_item }
+    Then { @stack.depth == 0 }
   }
 end
